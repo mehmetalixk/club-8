3f76d809a2e1060b1a48cc87a3bd8465ddbaaad8
Buğrahan <umutbugrahanozkan@gmail.com>
2021-04-16 16:33:02 +0300

    add initial project files

---
+++ src/main/java/com/example/demo384test/Demo384TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo384test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo384TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo384TestApplication.class, args);
+    }
+
+}
