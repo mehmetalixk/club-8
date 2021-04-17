4f6fd34246e8d87c866fdf4128c582e376ec27af
Buğrahan <umutbugrahanozkan@gmail.com>
2021-04-17 16:44:14 +0300

    update HomeController.java

--- src/main/java/com/example/demo384test/Application.java
+++ src/main/java/com/example/demo384test/Application.java
@@ -1,4 +1,4 @@
-package com.example.demo384test.service;
+package com.example.demo384test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/demo384test/HomeController.java
+++ src/main/java/com/example/demo384test/HomeController.java
@@ -1,12 +1,14 @@
-package com.example.demo384test.controller;
+package com.example.demo384test;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "home";
     }
