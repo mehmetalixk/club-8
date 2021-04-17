f77ce21575a87b62778d42a04dea61883e74ee86
mehmetalixk <mehmetalikorkmazx99@gmail.com>
2021-04-17 16:56:00 +0300

    model_n_view

--- src/main/java/com/example/demo384test/HomeController.java
+++ src/main/java/com/example/demo384test/HomeController.java
@@ -1,15 +1,15 @@
 package com.example.demo384test;
 
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class HomeController {
     @GetMapping("/")
-    public String index() {
-        return "home";
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        return modelAndView;
     }
 }
