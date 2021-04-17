4e86e56e437b9cfcdc978fe1cd0835b1324c00e1
Buğrahan <umutbugrahanozkan@gmail.com>
2021-04-17 15:19:00 +0300

    add initial home.html view

--- src/main/java/com/example/demo384test/service/Application.java
+++ src/main/java/com/example/demo384test/service/Application.java
@@ -1,10 +1,18 @@
-package com.example.demo384test;
+package com.example.demo384test.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Demo384TestApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Demo384TestApplication.class, args);
