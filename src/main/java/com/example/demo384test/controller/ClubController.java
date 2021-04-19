eb48160461f0fbd9685e39e0e7f8d9b724d1e5dd
Buğrahan <umutbugrahanozkan@gmail.com>
2021-04-19 21:22:07 +0300

    add dynamic paging for clubs

--- src/main/java/com/example/demo384test/controller/ClubController.java
+++ src/main/java/com/example/demo384test/controller/ClubController.java
@@ -3,26 +3,28 @@ package com.example.demo384test.controller;
 
 import com.example.demo384test.model.Club;
 import com.example.demo384test.repository.ClubRepository;
+import com.example.demo384test.repository.SubclubRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(path="/clubs")
 public class ClubController {
     @Autowired
     private ClubRepository clubRepository;
+    @Autowired
+    private SubclubRepository subclubRepository;
 
-    @PostMapping(path="/add")
-    public @ResponseBody
-    String addNewPermission (@RequestParam String name) {
-        Club n = new Club();
-        n.setTitle(name);
-        clubRepository.save(n);
-        return "Saved";
+    @RequestMapping(value="/{title}", method = RequestMethod.GET)
+    public ModelAndView getClubPage (@PathVariable String title, Model model) {
+        model.addAttribute("club", clubRepository.findByTitle(title));
+        return new ModelAndView("club");
     }
 
-    @GetMapping(path="/")
+    @GetMapping(value="/all")
     public @ResponseBody Iterable<Club> getAllClubs() {
         return clubRepository.findAll();
     }
