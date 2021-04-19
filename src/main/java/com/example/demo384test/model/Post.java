5e4b4366268e9d2fb8b95e01eab6b09ff4c3e373
Buğrahan <umutbugrahanozkan@gmail.com>
2021-04-19 19:21:03 +0300

    add post model

--- src/main/java/com/example/demo384test/model/Post.java
+++ src/main/java/com/example/demo384test/model/Post.java
@@ -1,2 +1,87 @@
-package com.example.demo384test.model;public class Post {
+package com.example.demo384test.model;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="posts")
+public class Post {
+    @javax.persistence.Id
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 20)
+    private String title;
+    @Column(nullable = false)
+    private Date date;
+    @Column(nullable = false)
+    private Timestamp timestamp;
+    @Column(nullable = false)
+    private String content;
+
+
+    @OneToOne
+    private Member member;
+
+    @OneToOne
+    private Subclub subclub;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Subclub getSubclub() {
+        return subclub;
+    }
+
+    public void setSubclub(Subclub subclub) {
+        this.subclub = subclub;
+    }
 }
