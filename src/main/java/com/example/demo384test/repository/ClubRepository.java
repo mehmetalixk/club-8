94251f8bd0770d9cf63229be88647939a7fe06e3
Buğrahan <umutbugrahanozkan@gmail.com>
2021-04-19 21:23:14 +0300

    add club query

--- src/main/java/com/example/demo384test/repository/ClubRepository.java
+++ src/main/java/com/example/demo384test/repository/ClubRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo384test.repository;
 
 import com.example.demo384test.model.Club;
+import com.example.demo384test.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface ClubRepository extends JpaRepository<Club, Long> {
+    @Query(value = "SELECT * FROM clubs WHERE TITLE = ?1 LIMIT 1", nativeQuery = true)
+    Club findByTitle(String title);
 
 
 }
--- src/main/java/com/example/demo384test/repository/SubclubRepository.java
+++ src/main/java/com/example/demo384test/repository/SubclubRepository.java
@@ -2,9 +2,9 @@ package com.example.demo384test.repository;
 
 import com.example.demo384test.model.Subclub;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface SubclubRepository extends JpaRepository<Subclub, Long> {
-
 
 }
