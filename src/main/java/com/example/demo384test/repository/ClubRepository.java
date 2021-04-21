a06e502b18ff9457f03697b3a1b3f4d44be968d3
mehmetalixk <mehmetalikorkmazx99@gmail.com>
2021-04-21 16:48:41 +0300

    admin_panel features added.

--- src/main/java/com/example/demo384test/repository/ClubRepository.java
+++ src/main/java/com/example/demo384test/repository/ClubRepository.java
@@ -5,10 +5,13 @@ import com.example.demo384test.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.ArrayList;
+
 
 public interface ClubRepository extends JpaRepository<Club, Long> {
     @Query(value = "SELECT * FROM clubs WHERE TITLE = ?1 LIMIT 1", nativeQuery = true)
     Club findByTitle(String title);
 
-
+    @Query(value = "SELECT TITLE FROM clubs", nativeQuery = true)
+    ArrayList<String> findAllTitles();
 }
