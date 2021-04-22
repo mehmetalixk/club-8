1061d5aa1cf676404abd06ba40cb7857ff071762
Tansel Altınel <b21591294@cs.hacettepe.edu.tr>
2021-04-22 13:45:53 +0300

    Added post test with member Added post test with subclub Added subclub test with parent club

--- src/test/java/com/example/demo384test/PostRepositoryTests.java
+++ src/test/java/com/example/demo384test/PostRepositoryTests.java
@@ -1,15 +1,19 @@
 package com.example.demo384test;
 
-import com.example.demo384test.model.Permission;
+import com.example.demo384test.model.Member;
 import com.example.demo384test.model.Post;
-import com.example.demo384test.repository.PermissionRepository;
+import com.example.demo384test.model.Subclub;
+import com.example.demo384test.repository.ClubRepository;
+import com.example.demo384test.repository.MemberRepository;
 import com.example.demo384test.repository.PostRepository;
+import com.example.demo384test.repository.SubclubRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.annotation.Rollback;
 
 import java.sql.Date;
@@ -30,8 +34,17 @@ public class PostRepositoryTests {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private ClubRepository clubRepository;
+
+    @Autowired
+    private SubclubRepository subclubRepository;
+
     @Test
-    public void testCreatePost(){
+    public void testCreatePost() {
         Post post = new Post();
         post.setContent("TEST CONTENT");
         post.setTitle("TEST TITLE");
@@ -44,5 +57,57 @@ public class PostRepositoryTests {
         Post existingPost = entityManager.find(Post.class, savedPost.getId());
 
         assertThat(existingPost.getTitle().equals(post.getTitle()));
+    }
+
+    @Test
+    public void testCreatePostWithMember() {
+        Member member = new Member();
+        member.setName("test");
+        member.setSurname("test");
+        member.setUsername("test");
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode("test");
+        member.setPassword(encodedPassword);
+        member.setEmailAddress("test");
+        member.setGender("test");
+        member.setBirthDate(new Date(Calendar.getInstance().getTime().getTime()));
+
+        Member savedMember = memberRepository.save(member);
+
+        Post post = new Post();
+        post.setContent("TEST CONTENT");
+        post.setTitle("TEST TITLE");
+        post.setDate(LocalDate.now());
+        post.setTimestamp(LocalTime.now());
+        post.setMemberUsername(savedMember.getUsername());
+        //post.setSubclubTitle("");
+
+        Post savedPost = postRepository.save(post);
+        Post existingPost = entityManager.find(Post.class, savedPost.getId());
+
+        assertThat(existingPost.getTitle().equals(post.getTitle()));
+        assertThat(existingPost.getMemberUsername().equals(savedMember.getUsername()));
+    }
+
+    @Test
+    public void testCreatePostWithSubClub() {
+        Subclub subclub = new Subclub();
+        subclub.setTitle("TEST SUBCLUB");
+
+        Subclub savedSubclub = subclubRepository.save(subclub);
+
+        Post post = new Post();
+        post.setContent("TEST CONTENT");
+        post.setTitle("TEST TITLE");
+        post.setDate(LocalDate.now());
+        post.setTimestamp(LocalTime.now());
+        //post.setMemberUsername("");
+        post.setSubclubTitle(savedSubclub.getTitle());
+
+        Post savedPost = postRepository.save(post);
+        Post existingPost = entityManager.find(Post.class, savedPost.getId());
+
+        assertThat(existingPost.getTitle().equals(post.getTitle()));
+        assertThat(existingPost.getSubclubTitle().equals(savedSubclub.getTitle()));
     }
 }
--- src/test/java/com/example/demo384test/SubClubRepositoryTests.java
+++ src/test/java/com/example/demo384test/SubClubRepositoryTests.java
@@ -1,9 +1,9 @@
 package com.example.demo384test;
 
 
-import com.example.demo384test.model.Permission;
+import com.example.demo384test.model.Club;
 import com.example.demo384test.model.Subclub;
-import com.example.demo384test.repository.PermissionRepository;
+import com.example.demo384test.repository.ClubRepository;
 import com.example.demo384test.repository.SubclubRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,12 +21,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SubClubRepositoryTests {
     @Autowired
     private TestEntityManager entityManager;
-
+    @Autowired
+    private ClubRepository clubRepository;
     @Autowired
     private SubclubRepository subclubRepository;
 
     @Test
-    public void testCreateSubClub(){
+    public void testCreateSubClub() {
         Subclub subclub = new Subclub();
         subclub.setTitle("TEST SUBCLUB");
 
@@ -34,5 +35,25 @@ public class SubClubRepositoryTests {
         Subclub existingSubclub = entityManager.find(Subclub.class, savedSubclub.getId());
 
         assertThat(existingSubclub.getTitle().equals(subclub.getTitle()));
+    }
+
+    @Test
+    public void testCreateSubClubWithParentClub() {
+
+        Club club = new Club();
+        club.setTitle("TEST CLUB");
+
+        Club savedClub = clubRepository.save(club);
+        Club existingClub = entityManager.find(Club.class, savedClub.getId());
+
+        Subclub subclub = new Subclub();
+        subclub.setTitle("TEST SUBCLUB");
+        subclub.setClubTitle(existingClub.getTitle());
+
+        Subclub savedSubclub = subclubRepository.save(subclub);
+        Subclub existingSubclub = entityManager.find(Subclub.class, savedSubclub.getId());
+
+        assertThat(existingSubclub.getTitle().equals(subclub.getTitle()));
+        assertThat(existingSubclub.getClubTitle().equals(subclub.getClubTitle()));
     }
 }
