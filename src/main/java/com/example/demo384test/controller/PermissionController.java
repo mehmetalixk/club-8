f04f889d536e6173d5422499087e43e925763e4c
Buğrahan <umutbugrahanozkan@gmail.com>
2021-04-22 13:16:08 +0300

    reorganize controllers

--- src/main/java/com/example/demo384test/controller/PermissionController.java
+++ src/main/java/com/example/demo384test/controller/PermissionController.java
@@ -6,14 +6,14 @@ import com.example.demo384test.repository.PermissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(path="/permissions")
 public class PermissionController {
     @Autowired
     private PermissionRepository permissionRepository;
 
-    @PostMapping(path="/add")
+    @PostMapping(path="/permissions/add")
     public @ResponseBody
     String addNewPermission (@RequestParam String name) {
         Permission n = new Permission();
@@ -22,8 +22,14 @@ public class PermissionController {
         return "Saved";
     }
 
-    @GetMapping(path="/all")
+    @GetMapping(path="/permissions/all")
     public @ResponseBody Iterable<Permission> getAllPermissions() {
         return permissionRepository.findAll();
+    }
+
+    @PostMapping("/process_add_permission")
+    public ModelAndView processAddPermission(Permission permission) {
+        permissionRepository.save(permission);
+        return new ModelAndView("success");
     }
 }
--- src/main/java/com/example/demo384test/controller/PostController.java
+++ src/main/java/com/example/demo384test/controller/PostController.java
@@ -1,23 +1,69 @@
 package com.example.demo384test.controller;
 
 
+import com.example.demo384test.detail.CustomMemberDetails;
+import com.example.demo384test.model.Member;
 import com.example.demo384test.model.Post;
+import com.example.demo384test.model.Subclub;
 import com.example.demo384test.repository.MemberRepository;
 import com.example.demo384test.repository.PostRepository;
 import com.example.demo384test.repository.SubclubRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(path="/posts")
 public class PostController {
     @Autowired
     private PostRepository postRepository;
+    @Autowired
+    private SubclubRepository subclubRepository;
+    @Autowired
+    private MemberRepository memberRepository;
 
-    @GetMapping(path="/all")
+    @GetMapping(path="/posts/all")
     public @ResponseBody Iterable<Post> getAllPosts() {
         return postRepository.findAll();
+    }
+
+    @GetMapping("/post")
+    public ModelAndView post(Model model) {
+        CustomMemberDetails principal = (CustomMemberDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal != null) {
+            boolean isAllowed = principal.hasPermission("ROLE_ADMIN") || principal.hasPermission("ROLE_USER");
+            if(!isAllowed)
+                return null;
+        }
+
+        model.addAttribute("post", new Post());
+        model.addAttribute("subclubList", subclubRepository.findAllTitles());
+        return new ModelAndView("post");
+    }
+
+    @PostMapping("/process_add_post")
+    public ModelAndView processAddPost(Post post) {
+        // READ subclub from the subclub repository
+        Subclub sc = subclubRepository.findByTitle(post.getSubclubTitle());
+        post.setDate(java.time.LocalDate.now());
+        post.setTimestamp(java.time.LocalTime.now());
+        // Get logged member
+        CustomMemberDetails principal = (CustomMemberDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Member m = memberRepository.findByUsername(principal.getUsername());
+        post.setMemberUsername(m.getUsername());
+        m.addPost(post);
+        sc.addPostToSubclub(post);
+
+        // CREATE a new post in post repository
+        postRepository.save(post);
+        // UPDATE the subclub with new post
+        subclubRepository.save(sc);
+        // UPDATE the member with new post
+        memberRepository.save(m);
+        return new ModelAndView("success");
+
     }
 
 }
