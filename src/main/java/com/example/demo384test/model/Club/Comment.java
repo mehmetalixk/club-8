a71f1a2bd0cff476d7d15b696adadba092fbc8ff
Buğrahan <umutbugrahanozkan@gmail.com>
2021-05-14 15:17:57 +0300

    Add Comment model

---
+++ src/main/java/com/example/demo384test/model/Club/Comment.java
@@ -0,0 +1,82 @@
+package com.example.demo384test.model.Club;
+
+import com.example.demo384test.model.Member;
+import com.example.demo384test.model.Post;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name="comments")
+public class Comment {
+    @javax.persistence.Id
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private LocalDate date;
+    @Column(nullable = false)
+    private LocalTime timestamp;
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Member.class)
+    @JoinColumn(name = "member_id", referencedColumnName = "id", nullable=false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Subclub.class)
+    @JoinColumn(name = "post_id", referencedColumnName = "id", nullable=false)
+    private Post post;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+
+}
