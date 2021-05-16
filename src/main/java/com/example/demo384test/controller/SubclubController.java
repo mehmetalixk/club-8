14d03c55fbc3a463f0db63a6bc078e3e1baf12af
Buğrahan <umutbugrahanozkan@gmail.com>
2021-05-16 19:35:46 +0300

    add subclub and club edit features

--- src/main/java/com/example/demo384test/controller/SubclubController.java
+++ src/main/java/com/example/demo384test/controller/SubclubController.java
@@ -6,9 +6,12 @@ import com.example.demo384test.model.Club.Subclub;
 import com.example.demo384test.repository.ClubRepository;
 import com.example.demo384test.repository.PostRepository;
 import com.example.demo384test.repository.SubclubRepository;
+import com.example.demo384test.request.ClubEditionRequest;
 import com.example.demo384test.request.SubclubCreationRequest;
+import com.example.demo384test.request.SubclubEditionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -46,6 +49,42 @@ public class SubclubController {
         System.out.println("All posts are removed from the post repository which are belong to " + sc.getTitle());
         subclubRepository.delete(sc);
         System.out.println(sc.getTitle() + " " + "removed from the repository");
+        return "redirect:/admin";
+    }
+
+    @RequestMapping(value="/clubs/{clubTitle}/{subclubID}/edit", method = RequestMethod.GET)
+    public ModelAndView getEditSubclub(@PathVariable String clubTitle, @PathVariable String subclubID, Model model) {
+        Long id = Long.parseLong(subclubID);
+        Club club = clubRepository.findByTitle(clubTitle);
+        /*Check if the club is exists*/
+        if(club == null) {
+            return new ModelAndView("error");
+        }
+
+        Subclub sc = subclubRepository.findByID(id);
+        /*Check if the sub-club is exists*/
+        if(sc == null) {
+            return new ModelAndView("error");
+        }
+        SubclubEditionRequest scer = new SubclubEditionRequest();
+        scer.setId(subclubID);
+        model.addAttribute("subclub", sc);
+        model.addAttribute("scer", scer);
+        return new ModelAndView("edit_subclub");
+    }
+
+    @PostMapping("/process_edit_subclub")
+    public String processEditSubclub(SubclubEditionRequest scer) {
+        Subclub sc = subclubRepository.findByID(Long.parseLong(scer.getId()));
+
+        /* If same subclub name already exists throw an error*/
+        if(subclubRepository.findByClubTitle(scer.getTitle(), sc.getClub().getTitle()) != null)
+            return "redirect:/error";
+
+        sc.setTitle(scer.getTitle());
+
+        subclubRepository.save(sc);
+
         return "redirect:/admin";
     }
 
