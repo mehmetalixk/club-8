8c7fbb72a775b077c9c7c3025b7a411eb72931ae
Buğrahan <umutbugrahanozkan@gmail.com>
2021-05-16 16:28:42 +0300

    add club deletion feature

--- src/main/java/com/example/demo384test/repository/ClubRepository.java
+++ src/main/java/com/example/demo384test/repository/ClubRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo384test.repository;
 
 import com.example.demo384test.model.Club.Club;
+import com.example.demo384test.model.Club.Subclub;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,4 +14,7 @@ public interface ClubRepository extends JpaRepository<Club, Long> {
 
     @Query(value = "SELECT TITLE FROM clubs", nativeQuery = true)
     ArrayList<String> findAllTitles();
+
+    @Query(value = "SELECT * FROM clubs WHERE id = ?1", nativeQuery = true)
+    Club findByID(Long id);
 }
