c095394928657e09cd4752cdc0c6b399840870e6
Buğrahan <umutbugrahanozkan@gmail.com>
2021-05-27 00:13:40 +0300

    add findByLevel query

--- src/main/java/com/example/demo384test/Logger/LogRepository.java
+++ src/main/java/com/example/demo384test/Logger/LogRepository.java
@@ -2,7 +2,11 @@ package com.example.demo384test.Logger;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LogRepository extends JpaRepository<Logger, Long> {
 
+
+    List<Logger> findByLevel(String level);
 
 }
